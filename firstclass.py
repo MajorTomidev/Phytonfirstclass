fe408b2baaaf4cb9e25195c3bb3305b9e0d2684e
tomidevops <oluwatomisinodukoya@RhEmAs-MacBook-Pro.local>
2022-09-28 12:50:22 +0100

    First Phython Class

---
+++ firstclass.py
@@ -0,0 +1,42 @@
+# print("Hello world") 
+# print("5")
+# print(type("4.5"))
+
+#variable assignment
+# my_num = 100
+# my_num2 = 10
+
+# print(my_num+my_num2)
+# print(my_num/my_num2)
+# print(my_num*my_num2)
+
+
+# my_name = "tomisin"
+
+# a = 4
+# b = 10
+
+# x = 4*b - 2*a
+# print(x)
+
+# numbers = 2+4+9+8+7+2
+
+# average = (2+4+9+8+7+2)/6
+# print(average)
+
+# a=4
+# b=7
+# c=2
+
+# numerator = (-b) + (4*a*c)**0.5
+# denominator = 2*a
+
+# x = numerator/denominator
+# print(x)
+
+tomi = 20
+y = 10
+
+x = tomi+y
+
+print(x)
